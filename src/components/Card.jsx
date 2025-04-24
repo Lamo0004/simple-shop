16109221d3875dd89b88294f9984baec0be465d5
Anarikke <anla0008@stud.kea.dk>
2025-04-24 10:12:24 +0200

    Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+const Card = () => {
+    return (<article>
+        <img src="https://via.placeholder.com/300x200" alt="Placeholder billede" />
+        <h2>Produktnavn</h2>
+        <p>Pris</p>
+        <Button />
+    </article>);
+}
+
+export default Card;
