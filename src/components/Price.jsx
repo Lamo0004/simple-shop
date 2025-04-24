f874885ee3186682071afd37dca50add710979c4
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-04-24 10:17:17 +0200

    price komponent uden style

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,10 +1,18 @@
+import ButtonNewSide from "./ButtonNewSide";
+
 const Price = () => {
   return (
     <div>
       <h2>Pris DKK</h2>
-      <p>Antal vare</p>
-      <p>Pris DKK</p>
-      <h3></h3>
+      <div className="flex">
+        <p>Antal vare</p>
+        <p>Pris DKK</p>
+      </div>
+      <div className="flex">
+        <h3>Total pris</h3>
+        <h3>Pris DKK</h3>
+      </div>
+      <ButtonNewSide title="GÅ TIL KASSEN" href="#"></ButtonNewSide>
     </div>
   );
 };
