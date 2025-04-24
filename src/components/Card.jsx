bd76781fdc46ef0192a67cbd8bfffe352b881e36
Anarikke <anla0008@stud.kea.dk>
2025-04-24 11:39:11 +0200

    Card + imageslider

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,32 @@
+import Image from "next/image";
+import Baggrund from "@/images/Baggrund.jpg";
+
 import Button from "./Button";
 
 const Card = () => {
-    return (<article className="max-w-sm bg-white overflow-hidden text-center p-4">
-        <img className="w-full h-auto rounded-md mb-4" src="/images/Baggrund.jpg" alt="Placeholder billede" />
-        <h2>Produktnavn</h2>
-        <p>Pris</p>
-        <Button />
-    </article>);
-}
+    return (
+        <article className="max-w-sm bg-white overflow-hidden text-center p-4">
+            <div className="w-full h-auto relative mb-4 rounded-md overflow-hidden">
+                <Image
+                    src={Baggrund}
+                    alt="Placeholder billede"
+                    layout="responsive"
+                    width={500}
+                    height={300}
+                    className="rounded-md"
+                />
+            </div>
+            <div className="text-left">
+                <h2>Produktnavn</h2>
+                <p>Pris</p>
+
+            </div>
+
+            <div className="text-right">
+                <Button />
+            </div>
+        </article>
+    );
+};
 
 export default Card;
