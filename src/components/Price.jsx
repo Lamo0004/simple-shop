1a4ee548c9ea0f46b281aa9f01a25948f8739b27
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-04-24 10:31:39 +0200

    price komponent

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -3,12 +3,12 @@ import ButtonNewSide from "./ButtonNewSide";
 const Price = () => {
   return (
     <div>
-      <h2>Pris DKK</h2>
-      <div className="flex">
+      <h2>Pris i alt</h2>
+      <div className="flex justify-between">
         <p>Antal vare</p>
         <p>Pris DKK</p>
       </div>
-      <div className="flex">
+      <div className="flex justify-between">
         <h3>Total pris</h3>
         <h3>Pris DKK</h3>
       </div>
