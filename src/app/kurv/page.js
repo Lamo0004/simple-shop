e39bf4171e104f1ce3382f368f9ba17706fc3bda
Mariawillow <mani0013@stud.kea.dk>
2025-04-24 12:48:26 +0200

    page

--- src/app/kurv/page.js
+++ src/app/kurv/page.js
@@ -2,12 +2,14 @@ import CartCard from "@/components/CartCard";
 import Price from "@/components/Price";
 
 const Kurv = () => {
-  return <article className="p-2">
-    <h1>Your Basket</h1>
+  return <article className="p-4">
+  <h1 className="text-2xl font-bold lg-4">Your Basket</h1>
+  
+  <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
     <CartCard />
     <Price />
-
-  </article>;
+  </div>
+</article>
 };
 
 export default Kurv;
