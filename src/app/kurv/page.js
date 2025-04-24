b5c63a04520a1e52573bc88d3a724bfe79c71334
Mariawillow <mani0013@stud.kea.dk>
2025-04-24 12:43:11 +0200

    price opdatering

--- src/app/kurv/page.js
+++ src/app/kurv/page.js
@@ -1,9 +1,11 @@
 import CartCard from "@/components/CartCard";
+import Price from "@/components/Price";
 
 const Kurv = () => {
-  return <article>
+  return <article className="p-2">
     <h1>Your Basket</h1>
     <CartCard />
+    <Price />
 
   </article>;
 };
