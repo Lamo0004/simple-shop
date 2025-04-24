37a97d2af477d3357b63be14762aeec175b3556b
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-04-24 10:03:13 +0200

    arrow komponent

---
+++ src/components/Arrow.jsx
@@ -0,0 +1,12 @@
+import { IoIosArrowBack } from "react-icons/io";
+
+const Arrow = () => {
+  return (
+    <div className="flex gap-4">
+      <IoIosArrowBack />
+      <p>Tilbage</p>
+    </div>
+  );
+};
+
+export default Arrow;
