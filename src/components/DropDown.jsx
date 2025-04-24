fecb613bc999dc6671edbad347379ad9791a79cc
Anarikke <anla0008@stud.kea.dk>
2025-04-24 14:08:47 +0200

    dropdown

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -5,6 +5,12 @@ import { IoIosArrowDown } from "react-icons/io";
 
 const Dropdown = ({ categories }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isActive, setIsActive] = useState("");
+
+  const setActiveCategory = (cat) => {
+    setIsActive(`${cat}`);
+    setIsOpen(false);
+  }
 
   return (
     <div className="relative inline-block text-left">
@@ -12,11 +18,10 @@ const Dropdown = ({ categories }) => {
         onClick={() => setIsOpen(!isOpen)}
         className="flex items-center gap-2 bg-gray-200 text-gray-800 font-medium py-2 px-4 rounded-md hover:bg-orange-400 hover:text-[#ededed] transition duration-200"
       >
-        Vælg kategori
+        {isActive ? isActive : "Vælg kategori"}
         <IoIosArrowDown
-          className={`transform transition-transform duration-200 ${
-            isOpen ? "rotate-180" : ""
-          }`}
+          className={`transform transition-transform duration-200 ${isOpen ? "rotate-180" : ""
+            }`}
         />
       </button>
 
@@ -24,9 +29,10 @@ const Dropdown = ({ categories }) => {
         <div className="absolute mt-2 w-48 rounded-md shadow-lg bg-white z-10">
           <ul className="py-1">
             {categories && categories.length > 0 ? (
-              categories.name.map((category, index) => (
+              categories.map((category, index) => (
                 <li
                   key={index} // Brug index som nøgle
+                  onClick={() => setActiveCategory(category.name)}
                   className="block px-4 py-2 text-gray-700 hover:bg-orange-400 hover:text-[#ededed] cursor-pointer transition duration-200 capitalize"
                 >
                   {category.name} {/* Vis kun "name" fra kategorien */}
