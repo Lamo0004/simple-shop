56b6e05dee7a5b0cdd08339e361ade6dc96cb6cc
Mariawillow <mani0013@stud.kea.dk>
2025-04-24 12:34:53 +0200

    opdatering af cartcard

--- src/app/kurv/page.js
+++ src/app/kurv/page.js
@@ -1,5 +1,11 @@
+import CartCard from "@/components/CartCard";
+
 const Kurv = () => {
-  return <article></article>;
+  return <article>
+    <h1>Your Basket</h1>
+    <CartCard />
+
+  </article>;
 };
 
 export default Kurv;
