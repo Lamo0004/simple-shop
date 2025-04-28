52c51f67ceab02509617c58bf260cf9a0b1d5947
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-04-28 12:09:20 +0200

    kurv ikon med tal

---
+++ src/components/CartIcon.jsx
@@ -0,0 +1,16 @@
+"use client";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import useCartStore from "@/app/store/cartStore";
+
+const CartIcon = () => {
+  const { items } = useCartStore();
+
+  return (
+    <div className="relative">
+      <HiOutlineShoppingBag size={35} />
+      {items.length > 0 && <span className="absolute bottom-0 right-0 bg-orange-400 text-white rounded-full text-xs px-2 py-1">{items.length}</span>}
+    </div>
+  );
+};
+
+export default CartIcon;
