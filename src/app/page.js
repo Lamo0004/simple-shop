04bd8a374693872432faca58e91dcbc755fd099b
Anarikke <anla0008@stud.kea.dk>
2025-04-28 11:38:21 +0200

    Img på forsiden

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,15 @@ import ButtonFrontpage from "@/components/ButtonFronpage";
 
 export default function Home() {
   return (
-    <div className="relative h-[88vh] sm:h-[94vh] overflow-hidden">
-      <Image src={frontPage} alt="Forside billede" quality={100} className="object-cover" layout="fill" />
-      <div className="absolute bottom-20 left-5 sm:left-20 lg:left-50 p-4">
-        <div>
-          <h1 className="text-4xl font-bold">10% Spring SALE☀️</h1>
-          <div className="my-8">
-            <ButtonFrontpage title="SHOP NU" href="/produkter" />
+    <div className="-mx-2">
+      <div className="relative h-[88vh] sm:h-[94vh] overflow-hidden">
+        <Image src={frontPage} alt="Forside billede" quality={100} className="object-cover" layout="fill" />
+        <div className="absolute bottom-20 left-5 sm:left-20 lg:left-50 p-4">
+          <div>
+            <h1 className="text-4xl font-bold">10% Spring SALE☀️</h1>
+            <div className="my-8">
+              <ButtonFrontpage title="SHOP NU" href="/produkter" />
+            </div>
           </div>
         </div>
       </div>
