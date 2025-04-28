bcd204df8461a1d7bcaf9eff207415465bf74310
Anarikke <anla0008@stud.kea.dk>
2025-04-28 14:03:38 +0200

    header

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import ButtonFrontpage from "@/components/ButtonFronpage";
 export default function Home() {
   return (
     <div className="-mx-2">
-      <div className="relative h-[88vh] sm:h-[94vh] overflow-hidden">
+      <div className="relative h-[88vh] sm:h-[92.5vh] overflow-hidden">
         <Image src={frontPage} alt="Forside billede" quality={100} className="object-cover" layout="fill" />
         <div className="absolute bottom-20 left-5 sm:left-20 lg:left-50 p-4">
           <div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,34 +5,36 @@ import CartIcon from "./CartIcon";
 
 const Header = () => {
   return (
-    <nav className="py-2 mx-auto max-w-screen-lg">
-      <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-6">
-        <div className="flex justify-between items-center w-full sm:w-auto">
-          {/* Logoet centreret på mobil */}
-          <div className="text-orange-400 text-3xl sm:hidden">
+    <div className="-mx-2 sticky top-0 z-50">
+      <nav className="py-2 w-full bg-[#ededed] shadow">
+        <div className="mx-auto max-w-screen-lg flex flex-col sm:flex-row sm:justify-between sm:items-center gap-6">
+          <div className="flex justify-between items-center w-full sm:w-auto">
+            {/* Logoet centreret på mobil */}
+            <div className="text-orange-400 text-3xl sm:hidden">
+              <Link className="font-logo" href="/">LAM.DK</Link>
+            </div>
+            <div className="flex items-center gap-6">
+              <li className="text-m hover:text-orange-400">
+                <Link href="/produkter">SHOP</Link>
+              </li>
+              <li className="hover:text-orange-400">
+                <Link href="/kurv">
+                  <CartIcon> </CartIcon>
+                </Link>
+              </li>
+            </div>
+          </div>
+          <div className="w-full sm:w-auto sm:order-[-1]">
+            <SearchBar />
+          </div>
+
+          {/* Logoet centreret på desktop */}
+          <div className="hidden sm:block text-orange-400 text-3xl sm:absolute sm:left-1/2 sm:transform sm:-translate-x-1/2">
             <Link className="font-logo" href="/">LAM.DK</Link>
           </div>
-          <div className="flex items-center gap-6">
-            <li className="text-m hover:text-orange-400">
-              <Link href="/produkter">SHOP</Link>
-            </li>
-            <li className="hover:text-orange-400">
-              <Link href="/kurv">
-                <CartIcon> </CartIcon>
-              </Link>
-            </li>
-          </div>
         </div>
-        <div className="w-full sm:w-auto sm:order-[-1]">
-          <SearchBar />
-        </div>
-
-        {/* Logoet centreret på desktop */}
-        <div className="hidden sm:block text-orange-400 text-3xl sm:absolute sm:left-1/2 sm:transform sm:-translate-x-1/2">
-          <Link className="font-logo" href="/">LAM.DK</Link>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
