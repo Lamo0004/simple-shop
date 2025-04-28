711b5d73bafed3becbb7a2d2bb02e09b73fbee81
Mariawillow <mani0013@stud.kea.dk>
2025-04-28 12:40:06 +0200

    button rigth

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,8 +9,9 @@ const Card = ({ product }) => {
 
 
   return (
-    //har tilføjet nth.. for at fortælle at der skal kun være på de første 6.
-    <article className="relative w-full max-w-xs flex flex-col bg-white overflow-hidden text-center p-4 rounded shadow ">
+    
+    
+    <article className="relative justify-center w-full max-w-xs flex flex-col bg-white overflow-hidden text-center p-4 rounded shadow ">
 {/* Hvis produktet har rabat, vis badge */}
 {roundedDiscount > 0 && (
           <div className="absolute top-2 left-2 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded">
@@ -26,13 +27,16 @@ const Card = ({ product }) => {
         </div>
 
         <div className="flex-1 text-left">
-          <h3>{product.title}</h3>
+          <h4>{product.title}</h4>
           <p>${product.price}</p>
         </div>
       </Link>{" "}
       {/* Slut Link komponenten */}
-      <div className="absolute bottom-4 right-4 mt-4 text-right">
+
+      <div className="mt-12 relative"> {/* Ændret fra absolute til relative */}
+      <div className="absolute bottom-4 right-4">
         <Button item={product}>Læg i kurv</Button> {/* Den funktionelle knap */}
+      </div>
       </div>
     </article>
   );
